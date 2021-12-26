c0c54101ecdfa27264cbd64ba24040660dfbd4fb
Antons Repins <dumperix1993@gmail.com>
2021-12-26 18:39:22 +0200

    moved to shared

---
+++ TerraformingShared/WorldStreaming/ClipmapCellPatches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorldStreaming;
+
+namespace Terraforming.WorldStreaming.ClipmapCellPatches
+{
+    [HarmonyPatch(typeof(ClipmapCell))]
+    [HarmonyPatch("BeginBuildLayers")]
+    [HarmonyPatch(new Type[] { typeof(MeshBuilder) })]
+    static class BeginBuildLayersPatch
+    {
+        static void Prefix(out ClipmapChunk __state, ClipmapChunk ___chunk)
+        {
+            var oldChunk = ___chunk;
+            __state = oldChunk;
+
+            if (oldChunk && oldChunk.gameObject)
+            {
+                UnityEngine.Object.Destroy(oldChunk.gameObject);
+            }
+        }
+    }
+}
