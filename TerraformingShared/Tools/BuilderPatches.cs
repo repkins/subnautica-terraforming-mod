d0f0224e9ffbd1475c47b3189ef25f4ac2a0c4ca
Antons Repins <dumperix1993@gmail.com>
2021-12-26 18:38:12 +0200

    merged patches

--- TerraformingShared/Tools/BuilderPatches.cs
+++ TerraformingShared/Tools/BuilderPatches.cs
@@ -64,4 +64,23 @@ namespace Terraforming.Tools.BuilderPatches
             }
         }
     }
+
+    [HarmonyPatch(typeof(Builder))]
+    [HarmonyPatch("GetObstacles")]
+    static class GetObstaclesPatch
+    {
+        static void Postfix(Vector3 position, Quaternion rotation, List<OrientedBounds> localBounds, List<GameObject> results)
+        {
+            if (Config.Instance.habitantModulesPartialBurying)
+            {
+                results.RemoveAll((gameObject) => Builder.IsObstacle(gameObject.GetComponent<Collider>()));
+            }
+
+            if (Config.Instance.destroyLargerObstaclesOnConstruction)
+            {
+                results.RemoveAll((gameObject) => gameObject.GetComponent<ConstructionObstacle>() != null);
+                results.RemoveAll((gameObject) => gameObject.GetComponent<ImmuneToPropulsioncannon>() != null);
+            }
+        }
+    }
 }
