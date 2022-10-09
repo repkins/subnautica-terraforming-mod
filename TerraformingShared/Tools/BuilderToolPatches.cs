83921e0b338b2b9730f7c97d750809b497bd7569
Antons Repins <dumperix1993@gmail.com>
2022-10-10 00:17:06 +0300

    Enabled construct "anywhere" regardless of config option.

--- TerraformingShared/Tools/BuilderToolPatches.cs
+++ TerraformingShared/Tools/BuilderToolPatches.cs
@@ -10,26 +10,18 @@ namespace TerraformingShared.Tools.BuilderToolPatches
     [HarmonyPatch(nameof(BuilderTool.Update))]
     public static class UpdatePatch
     {
+        public static bool storedEnabledDestroyingObstacles = Config.Instance.destroyLargerObstaclesOnConstruction;
+
         public static void Postfix()
         {
             if (GameInput.GetButtonDown(GameInput.Button.AltTool) || GameInput.GetButtonUp(GameInput.Button.AltTool))
             {
                 Config.Instance.destroyLargerObstaclesOnConstruction = !Config.Instance.destroyLargerObstaclesOnConstruction;
             }
-        }
-    }
 
-    [HarmonyPatch(typeof(BuilderTool))]
-    [HarmonyPatch(nameof(BuilderTool.UpdateCustomUseText))]
-    public static class UpdateCustomUseTextPatch
-    {
-        public static void Postfix(BuilderTool __instance)
-        {
-            if (Builder.isPlacing)
+            if (!GameInput.GetButtonHeld(GameInput.Button.AltTool))
             {
-                var obstaclesUseText = string.Format("Toggle Destroy Obstacles (Hold {0})", uGUI.FormatButton(GameInput.Button.AltTool));
-
-                __instance.customUseText = __instance.customUseText.Replace("\n", string.Format(", {0}\n", obstaclesUseText));
+                storedEnabledDestroyingObstacles = Config.Instance.destroyLargerObstaclesOnConstruction;
             }
         }
     }
@@ -43,7 +35,13 @@ namespace TerraformingShared.Tools.BuilderToolPatches
         {
             if (!constructable.constructed)
             {
-                var obstaclesUseText = string.Format("Toggle Destroy Obstacles (Hold {0})", uGUI.FormatButton(GameInput.Button.AltTool));
+                var enableText = "Enable Destroying Obstacles";
+                var disableText = "Disable Destroying Obstacles";
+
+                var storedDestroyingEnabled = UpdatePatch.storedEnabledDestroyingObstacles;
+
+                var obstaclesUseText = string.Format("{0} (Hold {1})", storedDestroyingEnabled ? disableText : enableText, uGUI.FormatButton(GameInput.Button.AltTool));
+
                 var handSubscriptText = HandReticle.main.textHandSubscript;
                 handSubscriptText = handSubscriptText.Insert(handSubscriptText.IndexOf(Environment.NewLine), string.Format(", {0}", obstaclesUseText));
 
