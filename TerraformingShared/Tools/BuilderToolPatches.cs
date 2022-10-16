74b028da16df945a8f1d11efa332b8e7a5c15f87
Antons Repins <dumperix1993@gmail.com>
2022-10-16 15:58:34 +0300

    destroyable obstacles highlighting.

--- TerraformingShared/Tools/BuilderToolPatches.cs
+++ TerraformingShared/Tools/BuilderToolPatches.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Terraforming;
+using UnityEngine.Rendering;
+using UnityEngine;
+using System.Collections;
 
 namespace TerraformingShared.Tools.BuilderToolPatches
 {
@@ -11,17 +14,126 @@ namespace TerraformingShared.Tools.BuilderToolPatches
     public static class UpdatePatch
     {
         public static bool storedEnabledDestroyingObstacles = Config.Instance.destroyLargerObstaclesOnConstruction;
+        static Material destroyableObstacleMat = null;
+
+        static bool emptyPrevTarget = true;
 
         public static void Postfix()
         {
+            if (destroyableObstacleMat == null)
+            {
+                destroyableObstacleMat = new Material(Builder.builderObstacleMaterial);
+                destroyableObstacleMat.SetColor(ShaderPropertyID._Tint, Color.red);
+            }
+
+            bool destroyToggled = false;
+            bool configChanged = false;
             if (GameInput.GetButtonDown(GameInput.Button.AltTool) || GameInput.GetButtonUp(GameInput.Button.AltTool))
             {
                 Config.Instance.destroyLargerObstaclesOnConstruction = !Config.Instance.destroyLargerObstaclesOnConstruction;
+                destroyToggled = true;
             }
 
             if (!GameInput.GetButtonHeld(GameInput.Button.AltTool))
             {
+                configChanged = storedEnabledDestroyingObstacles != Config.Instance.destroyLargerObstaclesOnConstruction;
                 storedEnabledDestroyingObstacles = Config.Instance.destroyLargerObstaclesOnConstruction;
+            }
+
+            bool isConstructableTarget = false;
+
+            Targeting.AddToIgnoreList(Player.main.gameObject);
+            Targeting.GetTarget(30f, out var targetObject, out var num);
+            if (targetObject != null)
+            {
+                var constructableBase = targetObject.GetComponentInParent<ConstructableBase>();
+                var constructable = targetObject.GetComponentInParent<Constructable>();
+
+                if (constructableBase != null && constructable != null && num <= constructable.placeMaxDistance)
+                {
+                    isConstructableTarget = true;
+
+                    if (destroyToggled || configChanged || emptyPrevTarget)
+                    {
+                        Builder.obstaclesBuffer.Clear();
+                        if (Config.Instance.destroyLargerObstaclesOnConstruction)
+                        {
+                            HighlightDestroyableObstacles(constructableBase);
+                        }
+                    }
+
+                    emptyPrevTarget = false;
+                }
+            }
+
+            if (!isConstructableTarget)
+            {
+                Builder.obstaclesBuffer.Clear();
+                emptyPrevTarget = true;
+            }
+        }
+
+        static void HighlightDestroyableObstacles(ConstructableBase constructableBase)
+        {
+            using (var obstacleListPool = Pool<ListPool<GameObject>>.Get())
+            {
+                var obstacleList = obstacleListPool.list;
+
+                using (var constructableBoundsListPool = Pool<ListPool<ConstructableBounds>>.Get())
+                {
+                    var constructableBoundsList = constructableBoundsListPool.list;
+                    constructableBase.GetComponentsInChildren(true, constructableBoundsList);
+
+                    using (var overlappedObjectsListPool = Pool<ListPool<GameObject>>.Get())
+                    {
+                        var overlappedObjectsList = overlappedObjectsListPool.list;
+
+                        foreach (var constructableBounds in constructableBoundsList)
+                        {
+                            var orientedBounds = OrientedBounds.ToWorldBounds(constructableBounds.transform, constructableBounds.bounds);
+                            overlappedObjectsList.Clear();
+                            Builder.GetOverlappedObjects(orientedBounds.position, orientedBounds.rotation, orientedBounds.extents, overlappedObjectsList);
+
+                            obstacleList.AddRange(overlappedObjectsList);
+                        }
+                    }
+                }
+
+                if (obstacleList.Count > 0)
+                {
+                    using (var materialListPool = Pool<ListPool<Material>>.Get())
+                    {
+                        var materialList = materialListPool.list;
+
+                        foreach (var obstacle in obstacleList)
+                        {
+                            if (!(obstacle.GetComponent<BaseCell>() != null))
+                            {
+                                Builder.sRenderers.Clear();
+                                obstacle.GetComponentsInChildren(Builder.sRenderers);
+                                foreach (var renderer in Builder.sRenderers)
+                                {
+                                    if (renderer.enabled && renderer.shadowCastingMode != ShadowCastingMode.ShadowsOnly && !(renderer is ParticleSystemRenderer))
+                                    {
+                                        renderer.GetSharedMaterials(materialList);
+                                        var shaderPassNum = 0;
+                                        foreach (var material in materialList)
+                                        {
+                                            shaderPassNum++;
+                                            if (!(material == null) && !Builder.shadersToExclude.Contains(material.shader) && material.renderQueue < 2450
+                                                && (!material.HasProperty(ShaderPropertyID._EnableCutOff)
+                                                || material.GetFloat(ShaderPropertyID._EnableCutOff) <= 0f)
+                                                && !material.IsKeywordEnabled("FX_BUILDING"))
+                                            {
+                                                Builder.obstaclesBuffer.DrawRenderer(renderer, destroyableObstacleMat, shaderPassNum);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
