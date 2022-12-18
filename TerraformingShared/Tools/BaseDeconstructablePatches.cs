425f7b0b21771268da2c505677ba5a2275530a43
Antons Repins <dumperix1993@gmail.com>
2022-12-18 16:27:09 +0200

    Cleanup of obsolete files.

--- TerraformingShared/Tools/BaseDeconstructablePatches.cs
+++
@@ -1,9 +0,0 @@
-﻿using HarmonyLib;
-using HarmonyLib.Tools;
-using System;
-using System.Collections.Generic;
-using System.Reflection.Emit;
-using System.Text;
-using Terraforming.Tools.BuilderPatches;
-using UnityEngine;
-
