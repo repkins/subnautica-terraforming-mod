76c9997b53260e532e508f78d40eae90a3470107
Antons Repins <dumperix1993@gmail.com>
2022-12-26 22:24:09 +0200

    stopped upgrading already upgraded saves

--- TerraformingShared/SaveLoad/UserStoragePCPatches.cs
+++ TerraformingShared/SaveLoad/UserStoragePCPatches.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using Terraforming;
 using Terraforming.WorldStreaming;
+using static ClipMapManager;
 
 #if !BelowZero
 namespace TerraformingShared.SaveLoad
@@ -67,6 +69,58 @@ namespace TerraformingShared.SaveLoad
         private static string[] GetEmptyArray()
         {
             return new string[] { };
+        }
+
+        [HarmonyPatch(nameof(UserStoragePC.UpgradeSaveDataAsync))]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> SkipOctrees(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase originalMethod)
+        {
+            var codeMatcherCursor = new CodeMatcher(instructions);
+
+            Action<CodeMatcher, ILGenerator>[] patchers =
+            {
+                RemoveOctreesPattern
+            };
+
+            try
+            {
+                foreach (var patcher in patchers)
+                {
+                    patcher.Invoke(codeMatcherCursor, generator);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.ToString());
+
+                codeMatcherCursor.ReportFailure(originalMethod, Logger.Error);
+
+                return instructions;
+            }
+
+            return codeMatcherCursor.InstructionEnumeration();
+        }
+
+        private static void RemoveOctreesPattern(CodeMatcher codeCursor, ILGenerator generator)
+        {
+            List<Label> labels;
+
+            codeCursor.Start();
+            codeCursor.MatchForward(useEnd: false,
+                new CodeMatch(OpCodes.Brtrue),
+                new CodeMatch(OpCodes.Ldloc_S),
+                new CodeMatch(OpCodes.Ldstr, "compiled-batch-*.optoctrees"),
+                new CodeMatch(OpCodes.Ldc_I4_1),
+                new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(Directory), nameof(Directory.EnumerateFiles), new[] { typeof(string), typeof(string), typeof(SearchOption) })),
+                new CodeMatch(OpCodes.Call)
+            );
+            codeCursor.ThrowIfInvalid("Could not find condition with call to Directory.EnumerateFiles uses octrees search pattern");
+
+            labels = codeCursor.Instruction.ExtractLabels();
+
+            codeCursor.RemoveInstructions(6);
+
+            codeCursor.Instruction.WithLabels(labels);
         }
 
         [HarmonyPatch(nameof(UserStoragePC.UpgradeSaveDataAsync))]
