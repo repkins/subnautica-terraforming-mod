88daebc716a297f70f98fb4c6f9fce78795d274f
Antons Repins <dumperix1993@gmail.com>
2022-12-27 17:57:41 +0200

    reverted

--- TerraformingShared/SaveLoad/UserStoragePCPatches.cs
+++ TerraformingShared/SaveLoad/UserStoragePCPatches.cs
@@ -121,6 +121,85 @@ namespace TerraformingShared.SaveLoad
 
             codeCursor.Instruction.WithLabels(labels);
         }
+
+        [HarmonyPatch(nameof(UserStoragePC.UpgradeSaveDataAsync))]
+        [HarmonyPostfix]
+        static void EnsureCompiledOctreesBackFromBackup(UserStoragePC __instance, string backupPath, ref UserStorageUtils.UpgradeOperation __result)
+        {
+            __result = EnsureCompiledOctreesRestoredFromBackupAsync(__instance, backupPath, __result);
+        }
+
+        private static UserStorageUtils.UpgradeOperation EnsureCompiledOctreesRestoredFromBackupAsync(UserStoragePC userStoragePC, string backupPath, UserStorageUtils.UpgradeOperation originalOperation)
+        {
+            if (!string.IsNullOrEmpty(backupPath) && Directory.Exists(backupPath))
+            {
+                string compiledOctreesDirName = BatchOctreesStreamerExtensions.CompiledOctreesDirName;
+
+                var saveContainersToRestore = new List<string>();
+                foreach (string saveSlotDir in Directory.GetDirectories(userStoragePC.savePath))
+                {
+                    string saveFileName = Path.GetFileName(saveSlotDir);
+                    string saveCompiledOctreesPath = Path.Combine(saveSlotDir, compiledOctreesDirName);
+                    string backupSaveCompiledOctreesPath = Path.Combine(backupPath, saveFileName, compiledOctreesDirName);
+
+                    // Restore save slot if exists in backup and is empty in target save location
+                    if (Directory.Exists(backupSaveCompiledOctreesPath) && !Directory.EnumerateFiles(saveCompiledOctreesPath).Any())
+                    {
+                        saveContainersToRestore.Add(saveSlotDir);
+                    }
+                }
+
+                var restoreOperation = new UserStorageUtils.UpgradeOperation
+                {
+                    itemsTotal = originalOperation.itemsTotal,
+                    itemsPrecessed = originalOperation.itemsPrecessed,
+                    result = originalOperation.result,
+                    errorMessage = originalOperation.errorMessage
+                };
+                UserStoragePC.ioThread.Enqueue(RestoreCompiledOctreesFromBackup, userStoragePC, new UserStoragePC.UpgradeWrapper(restoreOperation, userStoragePC.savePath, backupPath, saveContainersToRestore));
+                return restoreOperation;
+            }
+            else
+            {
+                Logger.Warning(string.Format("Backup dir is not created."));
+            }
+            return originalOperation;
+        }
+
+        private static void RestoreCompiledOctreesFromBackup(object owner, object state)
+        {
+            var upgradeWrapper = (UserStoragePC.UpgradeWrapper)state;
+            var upgradeOperation = (UserStorageUtils.UpgradeOperation)upgradeWrapper.operation;
+
+            string backupPath = upgradeWrapper.backupPath;
+            string compiledOctreesDirName = BatchOctreesStreamerExtensions.CompiledOctreesDirName;
+            foreach (var saveSlotPath in upgradeWrapper.containersToUpgrade)
+            {
+                string saveFileName = Path.GetFileName(saveSlotPath);
+                string backupSaveCompiledOctreesPath = Path.Combine(backupPath, saveFileName, compiledOctreesDirName);
+
+                if (Directory.Exists(backupSaveCompiledOctreesPath))
+                {
+                    try
+                    {
+                        string newSaveCompiledOctreesPath = Path.Combine(saveSlotPath, compiledOctreesDirName);
+                        UWE.Utils.CopyDirectory(backupSaveCompiledOctreesPath, newSaveCompiledOctreesPath);
+
+                        Logger.Info(string.Format("Compiled octrees restored from backup for save {0}.", saveFileName));
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Warning(string.Format("Failed to restore octrees at save {0}: {1}", saveFileName, ex.Message));
+                    }
+                }
+                else
+                {
+                    Logger.Warning(string.Format("Backup save compiled-octrees dir does not exist at: {0}.", backupSaveCompiledOctreesPath));
+                }
+            }
+
+            upgradeOperation.done = true;
+        }
     }
 }
 #endif
