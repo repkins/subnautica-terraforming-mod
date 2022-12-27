ef08ea8bb4d7ae79bf73d6dbedd2d43191861e8d
Antons Repins <dumperix1993@gmail.com>
2022-12-27 17:17:17 +0200

    fixes

--- TerraformingSN1/Properties/AssemblyInfo.cs
+++ TerraformingSN1/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Terraforming")]
-[assembly: AssemblyCopyright("Copyright © repkins 2022")]
+[assembly: AssemblyCopyright("Copyright © Antons \"repkins\" Repins 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
