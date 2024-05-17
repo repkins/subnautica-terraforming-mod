a7db6e3a40b4c2770f496bbbea72f05724e3af50
Antons Repins <dumperix1993@gmail.com>
2024-05-17 17:19:10 +0300

    Below Zero as standalone BepInEx plugin

---
+++ TerraformingBZ/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terraforming
+{
+    [BepInPlugin("subnautica-zero.repkins.terraforming", "TerraformingBZ", "1.4.4.1")]
+    public class Plugin: BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            MainPatcher.Patch();
+        }
+    }
+}
